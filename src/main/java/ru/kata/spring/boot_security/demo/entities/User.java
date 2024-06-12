68c11afa1375f8072dcf093d3c79da3488c331a8
Nikita Noskov <noskovnikita@lenta.ru>
2024-06-12 13:20:29 +0300

    Add repositories and correct username

--- src/main/java/ru/kata/spring/boot_security/demo/entities/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entities/User.java
@@ -14,11 +14,8 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
-    @Column(name = "first_name")
-    private String firstName;
-
-    @Column(name = "last_name")
-    private String lastName;
+    @Column(name = "username")
+    private String username;
 
     @Column(name = "email")
     private String email;
---
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package ru.kata.spring.boot_security.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.management.relation.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
