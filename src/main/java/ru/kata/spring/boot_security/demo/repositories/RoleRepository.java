19617bd1264694e05d0bb4c24496d830713df7e9
Nikita Noskov <noskovnikita@lenta.ru>
2024-06-12 13:31:32 +0300

    fix import "Role" class

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/RoleRepository.java
@@ -2,8 +2,7 @@ package ru.kata.spring.boot_security.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.management.relation.Role;
+import ru.kata.spring.boot_security.demo.entities.Role;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
